152a539bf7a5b5a7ced2a96e7729713cfbff8d02
athulyacherukunnath <37291081+Athulyacherukunnath@users.noreply.github.com>
2018-11-01 00:22:28 +0530

    Update bigger .c

--- bigger .c
+++ bigger .c
@@ -2,15 +2,15 @@
 main()
 {
 int a,b,c;
-printf("\n enter the value of a");
+printf("\n enter the value of a  :");
 scanf("%d",&a);
-printf("\n enter the value of b");
+printf("\n enter the value of b   :");
 scanf("%d",&b);
-printf("\n enter the value of c");
+printf("\n enter the value of c    :");
 scanf("%d",&c);
 if(a>b)
 {
-printf("\n %d is greater",a);
+printf("\n %d is greater",a); //if a is greater than b
 }
 else if(b>c)
 {
